1947f6723ef093778fc04044b4f069977ce84c3f
skumar1708 <itbhu.sk@gmail.com>
2025-04-04 16:53:15 +0530

    TDD Step 5: Write failing test for two numbers

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -8,3 +8,7 @@ test("should return the number itself when a single number is passed", () => {
   expect(add("1")).toBe(1);
 });
 
+
+test("should return the sum of two numbers", () => {
+  expect(add("1,5")).toBe(6);
+});
