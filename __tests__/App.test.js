b0b9754054351ef77a9e131b8d704af61d276860
skumar1708 <itbhu.sk@gmail.com>
2025-04-04 16:49:21 +0530

    TDD Step 3: Write failing test for a single number

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -3,3 +3,8 @@ import { add } from "../src/stringCalculator";
 test("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
 });
+
+test("should return the number itself when a single number is passed", () => {
+  expect(add("1")).toBe(1);
+});
+
