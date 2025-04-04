fce7ab0bf800579ead3e73bc3762ff043549057e
skumar1708 <itbhu.sk@gmail.com>
2025-04-04 17:07:48 +0530

    TDD Step 10: Handle new lines as delimiters in String Calculator

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,8 @@
 export function add(numbers) {
     if (numbers === "") return 0;
 
-    const numArray = numbers.split(",").map(num => parseInt(num));
+    // Replace new lines (`\n`) with commas
+    const numArray = numbers.replace(/\n/g, ",").split(",").map(num => parseInt(num));
 
     return numArray.reduce((sum, num) => sum + num, 0);
 }
