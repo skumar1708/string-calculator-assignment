244001331db379a49e25fa909f8437d25465b9d5
skumar1708 <itbhu.sk@gmail.com>
2025-04-04 17:01:03 +0530

    TDD Step 8: Handle multiple numbers in String Calculator

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,8 +1,7 @@
 export function add(numbers) {
     if (numbers === "") return 0;
-    if (!numbers.includes(",")) return parseInt(numbers);
 
     const numArray = numbers.split(",").map(num => parseInt(num));
-    
+
     return numArray.reduce((sum, num) => sum + num, 0);
 }
